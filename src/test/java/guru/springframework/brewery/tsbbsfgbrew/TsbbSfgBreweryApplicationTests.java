53fb7169ed810f02e9b5059a4cb93eab4aae0647
Leenawat <leenawat.pa@gmail.com>
2019-03-16 15:31:31 +0700

    first commit

---
+++ src/test/java/guru/springframework/brewery/tsbbsfgbrew/TsbbSfgBreweryApplicationTests.java
@@ -0,0 +1,17 @@
+package guru.springframework.brewery.tsbbsfgbrew;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+//@ExtendWith({SpringExtension.class})
+@SpringBootTest
+public class TsbbSfgBreweryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
